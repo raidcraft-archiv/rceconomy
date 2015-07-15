1210c1c9a8eecc01817051a8f8e4b87500a45b89
Michael Döring <michael.doering@interhyp.de>
2015-07-15 18:49:01 +0200

    #2095 - converted rceconomy conversation actions

---
+++ src/main/java/de/raidcraft/rceconomy/actions/RemoveMoneyAction.java
@@ -0,0 +1,35 @@
+package de.raidcraft.rceconomy.actions;
+
+import de.raidcraft.RaidCraft;
+import de.raidcraft.api.action.action.Action;
+import de.raidcraft.api.economy.BalanceSource;
+import de.raidcraft.api.economy.Economy;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+/**
+ * @author mdoering
+ */
+public class RemoveMoneyAction implements Action<Player> {
+
+    @Override
+    @Information(
+            value = "player.money.remove",
+            desc = "Removes money from the player account.",
+            conf = {
+                    "amount: <1g2s33k>",
+                    "details: [optional transaction details for the logs]"
+            },
+            aliases = {"money.remove", "playermoney.remove"}
+    )
+    public void accept(Player player, ConfigurationSection config) {
+
+        Economy economy = RaidCraft.getEconomy();
+        double amount = economy.parseCurrencyInput(config.getString("amount"));
+        if (config.isSet("details")) {
+            economy.substract(player.getUniqueId(), amount, BalanceSource.PLUGIN, config.getString("details"));
+        } else {
+            economy.substract(player.getUniqueId(), amount);
+        }
+    }
+}
