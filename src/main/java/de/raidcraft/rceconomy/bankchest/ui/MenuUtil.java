bc6be7cf2914a44a6c4d2f96b2fc2cf09af599d6
Philip <urban.philip@gmail.com>
2016-01-08 02:51:04 +0100

    fixed menu

--- src/main/java/de/raidcraft/rceconomy/bankchest/ui/MenuUtil.java
+++ src/main/java/de/raidcraft/rceconomy/bankchest/ui/MenuUtil.java
@@ -94,7 +94,7 @@ public class MenuUtil {
         }
 
         ChatColor sell = ChatColor.RESET;
-        if(!bankMaterial.isBuy()) {
+        if(!bankMaterial.isSell()) {
             sell = ChatColor.STRIKETHROUGH;
         }
 
